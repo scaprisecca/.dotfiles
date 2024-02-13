cad5e4d4216f32396a02731a2876bda3ec464058
Scott Caprisecca <caprisecca@gmail.com>
2024-02-12 20:35:26 -0700

    Added my nvim config files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,15 @@
+require "redman.options"
+require "redman.keymaps"
+require "redman.plugins"
+require('colorbuddy').colorscheme('cobalt2') -- Activate Cobalt2 theme
+-- require('onenord').setup() -- Activate OneNord theme
+-- vim.cmd[[colorscheme dracula]] -- Activate Dracula theme
+require "redman.cmp"
+require "redman.telescope"
+require "redman.treesitter"
+require "redman.lualine"
+require "redman.autopairs"
+require "redman.gitsigns"
+require "redman.toggleterm"
+require "redman.whichkey"
+require "redman.nvim-tree"
